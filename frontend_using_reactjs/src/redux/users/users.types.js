a9f7ba1a9103eaf1f39733e050754c569ef6f6c3
kangmasjuqi <marjuqi.rahmat@gmail.com>
2021-09-22 20:40:49 -0700

    Update users.types.js

--- frontend_using_reactjs/src/redux/users/users.types.js
+++ frontend_using_reactjs/src/redux/users/users.types.js
@@ -1,5 +1,6 @@
 export const ERRORS = 'ERRORS';
 export const GETUSERS = 'GETUSERS';
+export const GETUSERDETAIL = 'GETUSERDETAIL';
 export const CREATENEWUSER = 'CREATENEWUSER';
 export const EDITUSER = 'EDITUSER';
 export const REMOVEUSER = 'REMOVEUSER';
